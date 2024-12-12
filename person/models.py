427aa0adb006cb27b20abcb039b11e15a7076646
S.Abolfazl Eftekhari <s.a.eftekhari.b@gmail.com>
2024-12-12 23:54:36 +0330

    ref: change max length for category name

--- person/models.py
+++ person/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=64)
+    name = models.CharField(max_length=75)
     description = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
