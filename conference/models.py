0ce93512e9845ee1cab2f989ffb266b7c8cbc3f3
S.Abolfazl Eftekhari <s.a.eftekhari.b@gmail.com>
2024-12-12 23:16:15 +0330

    ref: implement conference model

--- conference/models.py
+++ conference/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Conference(models.Model):
+    name = models.CharField(help_text='Conference name', max_length=255)
+    description = models.TextField(help_text='Conference description', null=True, blank=True)
+    start_date = models.DateField(help_text='Conference start date')
+    end_date = models.DateField(help_text='Conference end date')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "conferences"
+
+    def __str__(self):
+        return self.name
