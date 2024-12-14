d6bf648b61b360b5dcedaa01bd3dec83e5b8f023
S.Abolfazl Eftekhari <s.a.eftekhari.b@gmail.com>
2024-12-14 21:33:13 +0330

    ref: add conference app in installed app and change database config

--- Hamayesh_Negar_django/settings.py
+++ Hamayesh_Negar_django/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'person'
+    'person',
+    'conference'
 ]
 
 MIDDLEWARE = [
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'Hamayesh_Negar_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "hn_db",
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
