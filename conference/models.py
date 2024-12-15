07f3cb7d9a00ea28d8deef65e6e66d833e16f236
S.Abolfazl Eftekhari <s.a.eftekhari.b@gmail.com>
2024-12-15 23:10:51 +0330

    ref: some changes in conference model

--- conference/models.py
+++ conference/models.py
@@ -2,15 +2,18 @@ from django.db import models
 
 
 class Conference(models.Model):
-    name = models.CharField(help_text='Conference name', max_length=255)
+    name = models.CharField(help_text='Conference name', max_length=75)
     description = models.TextField(help_text='Conference description', null=True, blank=True)
     start_date = models.DateField(help_text='Conference start date')
     end_date = models.DateField(help_text='Conference end date')
+    is_active = models.BooleanField(default=True)
+    created_by = models.ForeignKey('user.User', on_delete=models.SET_NULL, null=True, editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name_plural = "conferences"
+        ordering = ['-start_date']
 
     def __str__(self):
         return self.name
