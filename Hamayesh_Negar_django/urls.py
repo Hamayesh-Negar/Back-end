fcc7eb75c9e61e3a77e7c575804cd96bb3a78656
S.Abolfazl Eftekhari <s.a.eftekhari.b@gmail.com>
2024-12-26 13:55:28 +0330

    ref: add APIs urls

--- Hamayesh_Negar_django/urls.py
+++ Hamayesh_Negar_django/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
+
+from conference.views import ConferenceViewSet
+
+router = routers.DefaultRouter()
+router.register(r'conferences', ConferenceViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api/token/', obtain_auth_token),
 ]
