eaa41cdbdd005c93259d7c80a41840c8a3a4a0da
S.Abolfazl Eftekhari <s.a.eftekhari.b@gmail.com>
2025-01-03 01:27:06 +0330

    ref: add categories url

--- person/urls.py
+++ person/urls.py
@@ -1,12 +1,11 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from person.views import PersonViewSet
+from person.views import PersonViewSet, CategoryViewSet
 
 router = routers.DefaultRouter()
-router.register(r'persons', PersonViewSet)
-
-app_name = 'person'
+router.register(r'categories', CategoryViewSet, basename='category')
+router.register(r'persons', PersonViewSet, basename='person')
 
 urlpatterns = [
     path('', include(router.urls))
