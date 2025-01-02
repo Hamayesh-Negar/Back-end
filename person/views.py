adb9b904f1e640edc037c588a00d4ef18837a305
S.Abolfazl Eftekhari <s.a.eftekhari.b@gmail.com>
2025-01-03 01:27:26 +0330

    feat: implement CategoryViewSet base

--- person/views.py
+++ person/views.py
@@ -1,10 +1,11 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters
+from rest_framework import filters, status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
-from person.models import Person
-from person.serializers import PersonSerializer
+from person.models import Person, Category
+from person.serializers import PersonSerializer, CategorySerializer
 from user.permissions import IsHamayeshManager, IsSuperuser
 
 
@@ -23,3 +24,22 @@ class PersonViewSet(ModelViewSet):
         if conference_id:
             queryset = queryset.filter(conference_id=conference_id)
         return queryset
+
+
+class CategoryViewSet(ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated, IsHamayeshManager, IsSuperuser]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filtetset_fields = ['is_active']
+    search_fields = ['name', 'description']
+    ordering_fields = ['members_count']
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance.members.exists():
+            return Response(
+                {"detail": "Cannot delete category with existing members"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        return super().destroy(request, *args, **kwargs)
