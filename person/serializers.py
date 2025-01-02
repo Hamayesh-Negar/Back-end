f7f182a1ff0954945ca7a05e7d896201bc969e35
S.Abolfazl Eftekhari <s.a.eftekhari.b@gmail.com>
2025-01-03 01:04:47 +0330

    feat: add an action and a validator for category serializers

--- person/serializers.py
+++ person/serializers.py
@@ -4,9 +4,27 @@ from person.models import Person, Category
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    members_count = serializers.SerializerMethodField()
+
     class Meta:
         model = Category
-        fields = ['id', 'name', 'description', 'conference']
+        fields = ['id', 'conference', 'name', 'description', 'members_count', 'created_at', 'updated_at']
+        read_only_fields = ['created_at', 'updated_at']
+
+    @staticmethod
+    def get_members_count(obj):
+        return obj.members.count()
+
+    def validate(self, data):
+        if self.instance:
+            conference = data.get('conference', self.instance.conference)
+        else:
+            conference = data.get('conference')
+
+        if Category.objects.filter(conference=conference, name=data['name']).exclude(
+                id=getattr(self.instance, 'id', None)).exists():
+            raise serializers.ValidationError({"name": "Category with this name already exists in the conference"})
+        return data
 
 
 class PersonSerializer(serializers.ModelSerializer):
