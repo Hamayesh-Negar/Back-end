636bcaed644c6f5962aeb45490378b509d166505
S.Abolfazl Eftekhari <s.a.eftekhari.b@gmail.com>
2025-01-14 22:32:08 +0330

    feat: implement UserViewSet with custom permissions and filtering

--- user/views.py
+++ user/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import filters
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from user.models import User
+from user.permissions import IsSuperuser
+from user.serializers import UserSerializer
+
+
+class UserViewSet(ModelViewSet):
+    serializer_class = UserSerializer
+    permission_classes = [IsSuperuser]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['first_name', 'last_name', 'email', 'phone']
+    ordering_fields = ['first_name', 'last_name', 'date_joined']
+    ordering = ['-date_joined']
+
+    def get_queryset(self):
+        user = self.request.user
+        queryset = User.objects.all()
+
+        if not isinstance(user, User):
+            return queryset.none()
+
+        if user.is_superuser:
+            return queryset
+        elif user.user_type == User.UserType.HAMAYESH_MANAGER:
+            return queryset.filter(user_type=User.UserType.HAMAYESH_YAR)
+        else:
+            return queryset.filter(id=user.id)
