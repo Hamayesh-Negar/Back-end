82027d528775d0c64d1a4e9fc9bedcd6fd3f60bc
S.Abolfazl Eftekhari <s.a.eftekhari.b@gmail.com>
2025-01-14 22:33:32 +0330

    feat: update UserViewSet to use IsAuthenticated permission and dynamic serializer selection

--- user/views.py
+++ user/views.py
@@ -1,14 +1,15 @@
 from rest_framework import filters
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from user.models import User
 from user.permissions import IsSuperuser
-from user.serializers import UserSerializer
+from user.serializers import UserSerializer, UserBaseSerializer, UserCreateSerializer
 
 
 class UserViewSet(ModelViewSet):
-    serializer_class = UserSerializer
-    permission_classes = [IsSuperuser]
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated, IsSuperuser]
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
     search_fields = ['first_name', 'last_name', 'email', 'phone']
     ordering_fields = ['first_name', 'last_name', 'date_joined']
@@ -27,3 +28,10 @@ class UserViewSet(ModelViewSet):
             return queryset.filter(user_type=User.UserType.HAMAYESH_YAR)
         else:
             return queryset.filter(id=user.id)
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return UserCreateSerializer
+        elif self.action in ['update', 'partial_update']:
+            return UserBaseSerializer
+        return UserSerializer
