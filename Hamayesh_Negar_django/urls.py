49556a4cafb8c56d7d84a8ac27a5666654538bf9
S.Abolfazl Eftekhari <s.a.eftekhari.b@gmail.com>
2025-01-16 17:49:03 +0330

    feat, chore: add user URL routing and UserViewSet for user management

--- Hamayesh_Negar_django/urls.py
+++ Hamayesh_Negar_django/urls.py
@@ -20,6 +20,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('user/', include('user.urls')),
     path('api/v1/', include('conference.urls')),
     path('api/v1/', include('person.urls')),
     path('api/token/', obtain_auth_token),
