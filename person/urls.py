a88ab5d6deab42d52839f1df440ef99a84940070
S.Abolfazl Eftekhari <s.a.eftekhari.b@gmail.com>
2025-01-20 20:21:56 +0330

    feat: add nested routing for conference-related views in URLs

--- person/urls.py
+++ person/urls.py
@@ -1,5 +1,5 @@
 from django.urls import include, path
-from rest_framework import routers
+from rest_framework_nested import routers
 
 import conference.views
 from person.views import PersonViewSet, CategoryViewSet, TaskViewSet, PersonTaskViewSet
@@ -11,6 +11,12 @@ router.register(r'persons', PersonViewSet, basename='person')
 router.register(r'tasks', TaskViewSet, basename='task')
 router.register(r'person_tasks', PersonTaskViewSet, basename='person_task')
 
+conference_router = routers.NestedDefaultRouter(router, r'conferences', lookup='conference')
+conference_router.register(r'persons', PersonViewSet, basename='conference-persons')
+conference_router.register(r'categories', CategoryViewSet, basename='conference-categories')
+conference_router.register(r'tasks', TaskViewSet, basename='conference-tasks')
+
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('', include(conference_router.urls)),
 ]
