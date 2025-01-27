876e8f66de0b2dffa77aea84f469b3a1134ce112
S.Abolfazl Eftekhari <s.a.eftekhari.b@gmail.com>
2025-01-27 22:00:09 +0330

    feat: add slug field and auto-generate slugs for Conference model

--- conference/models.py
+++ conference/models.py
@@ -2,10 +2,12 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils import timezone
+from django.utils.text import slugify
 
 
 class Conference(models.Model):
     name = models.CharField(help_text='Conference name', max_length=75)
+    slug = models.SlugField(max_length=100, unique=True, blank=True, help_text='Conference slug')
     description = models.TextField(help_text='Conference description', null=True, blank=True)
     start_date = models.DateField(help_text='Conference start date')
     end_date = models.DateField(help_text='Conference end date')
@@ -23,6 +25,14 @@ class Conference(models.Model):
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.slug)
+            org_slug = self.slug
+            if Conference.objects.filter(slug=self.slug).exists():
+                self.slug = f"{org_slug}-{self.id}"
+        super().save(*args, **kwargs)
 
 
 @receiver(post_save, sender=Conference)
