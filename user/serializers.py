e1f12a11783bd943061fe1c96e70b40e689bc92e
S.Abolfazl Eftekhari <s.eftekhari.dev@gmail.com>
2025-08-18 19:10:05 +0330

    fix: delete user_type field

--- user/serializers.py
+++ user/serializers.py
@@ -10,7 +10,7 @@ class UserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'first_name', 'last_name',
-                  'phone', 'user_type', 'is_active', 'date_joined']
+                  'phone', 'is_active', 'date_joined']
 
 
 class UserBaseSerializer(ModelSerializer):
